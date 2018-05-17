6402ed8bb064652ce500194a64a92a6f4c84be3c
Trystan Eveno <trystan.eveno@lephare.com>
2018-05-17 19:11:33 +0200

    start loader

--- src/react/utils/Routes.js
+++ src/react/utils/Routes.js
@@ -4,7 +4,31 @@ import { Route, Switch, HashRouter } from 'react-router-dom';
 import Home from './../pages/Home';
 
 class Routes extends Component {
-  render() {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded : false
+    };
+  }
+
+  componentDidMount() {
+    const me = this,
+      DELAY_LOADER = 5000;
+
+    if (sessionStorage.getItem('loaded') === 'false' || !sessionStorage.getItem('loaded')) {
+      sessionStorage.setItem('loaded', 'false');
+
+      if (sessionStorage.getItem('loaded') === 'falses') {
+        setTimeout(() => {
+          sessionStorage.setItem('loaded', 'true');
+          me.setState({ loaded: true });
+        }, DELAY_LOADER);
+      }
+    }
+  }
+
+  renderLoaded() {
     return (
       <HashRouter>
         <Switch>
@@ -13,6 +37,26 @@ class Routes extends Component {
         </Switch>
       </HashRouter>
     );
+  }
+
+  renderNotLoaded() {
+    return (
+      <section id="Loading">
+        <div className="logo">
+
+        </div>
+      </section>
+    );
+  }
+
+  render() {
+    const me = this;
+
+    if (sessionStorage.getItem('loaded') === 'true') {
+      return me.renderLoaded();
+    } else {
+      return me.renderNotLoaded();
+    }
   }
 }
 
